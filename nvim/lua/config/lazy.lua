0bc72fc17d5cd57666b3691e182a245e3a39f9e5
kraja <kraja@pop-os.localdomain>
2023-01-07 18:24:23 +0100

    preparing to swap config for lazygit

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,13 +1,8 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"--single-branch",
-		"git@github.com:folke/lazy.nvim.git",
-		lazypath,
-	})
+  -- bootstrap lazy.nvim
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
-vim.opt.runtimepath:prepend(lazypath)
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup("config.plugins")
