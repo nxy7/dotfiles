12330f4d06548b49734e2d62edb2fc38b54ab3e0
nxyt <lolnoxy@gmail.com>
2023-03-29 16:49:27 +0200

    packages split into separate file

--- home.nix
+++ home.nix
@@ -4,45 +4,7 @@
     homeDirectory = "/home/" + username;
     stateVersion = "23.05";
 
-    packages = with pkgs; [
-      nixfmt
-      cargo-espflash
-      bun
-      gitoxide
-      sqlc
-      htop
-      sccache
-      nodePackages_latest.ts-node
-      gopls
-      rust-analyzer
-      python39
-      qmk
-      google-cloud-sdk-gce
-      # rust support
-      rustup
-      # golang support
-      go
-      # javascript packages
-      nodejs
-      deno
-      yarn
-      nodePackages.pnpm
-
-      # all stuff necesarry for helix lsp
-      nodePackages.typescript
-      nodePackages.typescript-language-server
-      nodePackages.prettier
-      # other languages
-      julia
-      php
-
-      # dev setup
-      gcc
-      # binutils-unwrapped_2_38
-      lazygit
-      tmux
-      gh
-    ];
+    packages = import ./packages.nix pkgs;
   };
 
   # imports = [ ./helix.nix ];
---
+++ packages.nix
@@ -0,0 +1,40 @@
+{ pkgs }:
+with pkgs; [
+  nixfmt
+  cargo-espflash
+  bun
+  gitoxide
+  sqlc
+  htop
+  sccache
+  nodePackages_latest.ts-node
+  gopls
+  rust-analyzer
+  python39
+  qmk
+  google-cloud-sdk-gce
+  # rust support
+  rustup
+  # golang support
+  go
+  # javascript packages
+  nodejs
+  deno
+  yarn
+  nodePackages.pnpm
+
+  # all stuff necesarry for helix lsp
+  nodePackages.typescript
+  nodePackages.typescript-language-server
+  nodePackages.prettier
+  # other languages
+  julia
+  php
+
+  # dev setup
+  gcc
+  # binutils-unwrapped_2_38
+  lazygit
+  tmux
+  gh
+]
