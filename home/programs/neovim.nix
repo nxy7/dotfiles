a45e1101fa4ab74e454df8b527e693873ec3cd12
nxyt <lolnoxy@gmail.com>
2023-06-30 21:08:35 +0200

    neovim added

---
+++ home/programs/neovim.nix
@@ -0,0 +1,20 @@
+let
+  # settings = builtins.fromTOML (builtins.readFile ./../helix/config.toml);
+
+  # languages.language = [
+  #   {
+  #     name = "tsx";
+  #     auto-format = true;
+  #   }
+  #   {
+  #     name = "nix";
+  #     auto-format = true;
+  #     # language-server = {command = "rnix-lsp"; };
+  #     formatter = { command = "nixfmt"; };
+  #   }
+  # ];
+in {
+  enable = true;
+  # inherit settings;
+  # inherit languages;
+}
