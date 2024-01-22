4d245c71650ace5daa0dd44dc4822f4579d9120f
nxy7 <lolnoxy@gmail.com>
2024-01-22 23:49:15 +0100

    preparing to use hyprland in the future
    Signed-off-by: nxy7 <lolnoxy@gmail.com>

--- system/audio.nix
+++ system/audio.nix
@@ -1,12 +1,23 @@
 { pkgs, ... }: {
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
   };
+  # environment.etc = {
+  #   "pipewire/pipewire.conf.d/92-low-latency.conf".text = ''
+  #     context.properties = {
+  #       default.clock.rate = 48000
+  #       default.clock.quantum = 32
+  #       default.clock.min-quantum = 32
+  #       default.clock.max-quantum = 32
+  #     }
+  #   '';
+  # };
 
   services.avahi.enable = true;
   services.avahi.nssmdns = true;
--- system/hyprland.nix
+++ system/hyprland.nix
@@ -1,4 +1,25 @@
-{ pkgs, ... }: {
-  programs.hyprland.enable = true;
+{ pkgs, inputs, ... }: {
+  programs.hyprland = {
+    enable = true;
+    enableNvidiaPatches = true;
+    xwayland = true;
+    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+  };
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment.systemPackages = with pkgs; [
+    waybar
+    mako
+    libnotify
+
+    hyprpaper
+    rofi-wayland
+    # swaybg
+    # wpaperd
+    # mpvpaper
+    # eww
+  ];
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+  };
 }
